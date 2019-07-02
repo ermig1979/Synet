36b037cad87305e5f8c80efaf859d7ad6c0e1c1e
Yermalayeu Ihar <ermig@tut.by>
2019-07-02 15:00:23 +0300

    *update 3rd/Simd to latest.

---
+++ prj/sh/UpdateCopyrights.sh
@@ -0,0 +1,23 @@
+#set -x
+
+function UpdateCopyrights () {
+directory=$1
+cd ${directory}
+git ls-tree -r --name-status HEAD | while read file_name; do
+  last_update_year=$(git log -1 --date=short --format="%ad" -- $file_name)
+  last_update_year=${last_update_year:0:4}
+  copyright_year=$(sed '4q;d' ${file_name})
+  copyright_year=${copyright_year#*-}
+  copyright_year=${copyright_year:0:4}
+  if [ "${last_update_year}" != "${copyright_year}" ] && [ "${copyright_year:0:2}" == "20" ]; then 
+    echo "update ${copyright_year} to ${last_update_year} in ${file_name}"
+    sed -i '4s/'"-${copyright_year}"'/'"-${last_update_year}"'/g' ${file_name}
+  else
+    printf "scan ${directory}/${file_name}                            \r"
+  fi
+done
+cd ~-
+}
+
+UpdateCopyrights ../../src/Synet
+#UpdateCopyrights ../../src/Test
