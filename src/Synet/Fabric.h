83c4229fdcefc15afcab9a2d8c53d36f1e9a25cc
Yermalayeu Ihar <ermig@tut.by>
2023-07-11 10:04:49 +0300

    *refactoring of ArgMaxLayer.

--- src/Synet/Fabric.h
+++ src/Synet/Fabric.h
@@ -106,7 +106,7 @@ namespace Synet
             switch (param.type())
             {
             case LayerTypeAdd: return new AddLayer<T>(param, context, method);
-            case LayerTypeArgMax: return new ArgMaxLayer<T>(param, context);
+            case LayerTypeArgMax: return new ArgMaxLayer(param, context);
             case LayerTypeBias: return new BiasLayer<T>(param, context);
             case LayerTypeBinaryOperation: return new BinaryOperationLayer<T>(param, context);
             case LayerTypeBroadcast: return new BroadcastLayer<T>(param, context);
--- src/Synet/Layers/ArgMaxLayer.h
+++ src/Synet/Layers/ArgMaxLayer.h
@@ -24,64 +24,25 @@
 
 #pragma once
 
-#include "Synet/Common.h"
 #include "Synet/Layer.h"
-#include "Synet/Utils/Math.h"
 
 namespace Synet
 {
-    template <class T> class ArgMaxLayer : public Synet::Layer<T>
+    class ArgMaxLayer : public Synet::Layer<float>
     {
     public:
-        typedef T Type;
-        typedef Layer<T> Base;
+        typedef Layer<float> Base;
         typedef typename Base::TensorPtrs TensorPtrs;
 
-        ArgMaxLayer(const LayerParam & param, Context* context)
-            : Base(param, context)
-        {
-        }
+        ArgMaxLayer(const LayerParam& param, Context* context);
 
-        virtual bool Reshape(const TensorPtrs& src, const TensorPtrs& buf, const TensorPtrs& dst)
-        {
-            assert(src.size() == 1);
-            const ArgMaxParam & param = this->Param().argMax();
-            Shape shape = src[0]->Shape();
-            if (param.keepDims() || shape.size() == 1)
-                shape[param.axis()] = 1;
-            else
-                shape.erase(shape.begin() + param.axis());
-            _outer = src[0]->Size(0, param.axis());
-            _count = src[0]->Axis(param.axis());
-            _inner = src[0]->Size(param.axis() + 1);
-
-            dst[0]->As64i().Reshape(shape, src[0]->Format());
-            this->UsePerfStat();
-            return true;
-        }
+        virtual bool Reshape(const TensorPtrs& src, const TensorPtrs& buf, const TensorPtrs& dst);
 
     protected:
-        virtual void ForwardCpu(const TensorPtrs & src, const TensorPtrs & buf, const TensorPtrs & dst)
-        {
-            const T * pSrc = src[0]->CpuData();
-            int64_t * pDst = dst[0]->As64i().CpuData();
-            memset(pDst, 0, _outer * _inner * sizeof(int64_t));
-            for (size_t o = 0; o < _outer; ++o)
-            {
-                for (size_t c = 0; c < _count; ++c)
-                {
-                    for (size_t i = 0; i < _inner; ++i)
-                    {
-                        if (pSrc[(o * _count + c) * _inner + i] > pSrc[(o * _count + pDst[o * _inner + i]) * _inner + i])
-                        {
-                            pDst[o * _inner + i] = c;
-                        }
-                    }
-                }
-            }
-        }
+        virtual void ForwardCpu(const TensorPtrs& src, const TensorPtrs& buf, const TensorPtrs& dst);
 
     private:
+        TensorType _srcT;
         size_t _outer, _count, _inner;
     };
 }
