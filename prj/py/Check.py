6a9b94ab5c3a629ff0da2f0f2d08b6474300efde
Yermalayeu Ihar <ermig@tut.by>
2023-09-05 17:14:27 +0300

    +add GetTestList function to Check script.

--- prj/py/Check.py
+++ prj/py/Check.py
@@ -1,16 +1,28 @@
 import argparse
 import os
+from datetime import datetime
 
 ###################################################################################################
 
+class Test():
+	def __init__(self):
+		self.framework = ""
+		self.group = ""
+		self.name = ""
+		self.image = ""
+		self.batch = False
+		self.path = ""
+		
+###################################################################################################
+
 def GetTestList(args):
-	listPath = args.src + "/tests.txt"
+	tests = []
+	listPath = args.src + os.path.sep + "tests.txt"
 	print(listPath)
 	if not os.path.isfile(listPath):
-		print("Test list '", listPath, "' is not exist!")
-		return False
+		print("Test list '{}' is not exist!".format(listPath))
+		return False, tests
 	listFile = open(listPath, "r")
-	count = 0
 	while True:
 		line = listFile.readline()
 		if not line:
@@ -18,9 +30,40 @@ def GetTestList(args):
 		vals = line.split()
 		if len(vals) < 5 :
 			continue
-		print("Test {}: {}".format(count, line.strip()))
-		count += 1
+		test = Test()
+		test.framework = vals[0]
+		test.group = vals[1]
+		test.name = vals[2]
+		test.image = vals[3]
+		test.batch = vals[4]
+		
+		if not (test.framework == "onnx" or test.framework == "inference_engine") :
+			return False, tests
+		
+		test.path = test.framework
+		if test.group != "root" :
+			test.path += os.path.sep + test.group
+		test.path += os.path.sep + test.name
+		if args.include != "" and test.path.find(args.include) == -1:
+			continue
+		if args.exclude != "" and test.path.find(args.exclude) != -1:
+			continue
+		
+		tests.append(test)
 	listFile.close()
+	return True, tests
+
+###################################################################################################
+
+def RunTest(args, dstPath, test):
+	binPath = args.bin + os.path.sep + "test_" + test.framework
+	if not os.path.isfile(binPath):
+		print("Binary '{}' is not exist!".format(binPath))
+		return False
+	testPath = args.src + os.path.sep + test.path
+	if not os.path.isdir(testPath):
+		print("Test directory '{}' is not exist!".format(testPath))
+		return False
 	return True
 
 ###################################################################################################
@@ -29,16 +72,47 @@ def main():
 
 	parser = argparse.ArgumentParser(prog="Check", description="Synet tests check script.")
 	parser.add_argument("-s", "--src", help="Tests data path.", required=False, type=str, default="./data")
-	parser.add_argument("-d", "--dst", help="Output tests path.", required=False, type=str, default="../test")
+	parser.add_argument("-b", "--bin", help="Tests binary path.", required=False, type=str, default="./build")
+	parser.add_argument("-d", "--dst", help="Output tests path.", required=False, type=str, default="../test/check")
 	parser.add_argument("-t", "--threads", help="Tests threads number.", required=False, type=int, default=1)
 	parser.add_argument("-f", "--fast", help="Fast check flag (no conversion, small image count).", required=False, type=bool, default=False)
 	parser.add_argument("-i", "--include", help="Include tests filter.", required=False, type=str, default="")
 	parser.add_argument("-e", "--exclude", help="Exclude tests filter.", required=False, type=str, default="")
 	args = parser.parse_args()
 	
+	if not os.path.isdir(args.src):
+		print("Test data directory '", args.src, "' is not exist!")
+		return 1
+
+	if not os.path.isdir(args.bin):
+		print("Binary directory '", args.bin, "' is not exist!")
+		return 1
+	
+	now = datetime.now();
+	dstPath = args.dst + "_" + str(now.year) + "_" + str(now.month) + "_" + str(now.day) + "__" + str(now.hour) + "_" + str(now.minute)
+	print("Test output directory: ", dstPath)
+	if not os.path.isdir(dstPath):
+		try:
+			os.makedirs(dstPath)
+		except OSError as error: 
+			print("Can't create output directory '" , dstPath, "' !")
+			return 1
+	
 	print("Synet Check:", args)	
 	
-	GetTestList(args)
+	result : bool
+	result, tests = GetTestList(args)
+	if not result:
+		return 1
+	
+	count = 0
+	for test in tests:
+		print("Test {}: {} {} {} {} {} ".format(count, test.framework, test.group, test.name, test.image, test.batch))
+		if not RunTest(args, dstPath, test):
+			return 1
+		count += 1
+
+	return 0
 	
 ###################################################################################################
 	
