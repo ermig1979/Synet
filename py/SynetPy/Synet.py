887e4ee124b18e9a5afed353b1f020f3217ed33b
Yermalayeu Ihar <ermig@tut.by>
2024-01-25 12:33:04 +0300

    *update SynetPy.

--- py/SynetPy/Synet.py
+++ py/SynetPy/Synet.py
@@ -1,3 +1,24 @@
+###################################################################################################
+# Synet Framework (http://github.com/ermig1979/Synet).
+#
+# Copyright (c) 2018-2024 Yermalayeu Ihar.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+# associated documentation files (the "Software"), to deal in the Software without restriction, 
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or 
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###################################################################################################
+
 import argparse
 import os
 import ctypes
@@ -12,10 +33,17 @@ Synet = sys.modules[__name__]
 
 ###################################################################################################
 
+## @ingroup python
+# A wrapper around %Synet Library API.
 class Lib():
 	lib : ctypes.CDLL
 	
-	def Init(dir: str):
+	## Initializes Synet.Lib class (loads %Synet Library binaries).
+	# @note This method must be called before any using of this class.
+	# @param dir - a directory with %Synet Library binaries (Synet.dll or libSynet.so). By default it is empty string. That means that binaries will be searched in the directory with current Python file.
+	def Init(dir = ""):
+		if dir == "" :
+			dir = pathlib.Path(__file__).parent.resolve()
 		if not os.path.isdir(dir):
 			raise Exception("Directory '{0}' with binaries is not exist!".format(dir))
 		name : str
@@ -89,6 +117,8 @@ class Lib():
 		Lib.lib.SynetTensorData.argtypes = [ ctypes.c_void_p ]
 		Lib.lib.SynetTensorData.restype = ctypes.c_void_p
 		
+	## Gets version of %Synet Framework.
+	# @return A string with version.
 	def Version() -> str: 
 		ptr = Lib.lib.SynetVersion()
 		return str(ptr, encoding='utf-8')
--- py/SynetPy/Test.py
+++ py/SynetPy/Test.py
@@ -1,3 +1,24 @@
+###################################################################################################
+# Synet Framework (http://github.com/ermig1979/Synet).
+#
+# Copyright (c) 2018-2024 Yermalayeu Ihar.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+# associated documentation files (the "Software"), to deal in the Software without restriction, 
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or 
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###################################################################################################
+
 import argparse
 import os
 import ctypes
@@ -11,59 +32,67 @@ import Simd
 
 def main():
 	parser = argparse.ArgumentParser(prog="Synet", description="Synet Python Wrapper.")
-	parser.add_argument("-b", "--bin", help="Directory with binary files.", required=False, type=str, default=".")
-	parser.add_argument("-m", "--model", help="Path to model file.", required=False, type=str, default="synet.xml")
-	parser.add_argument("-w", "--weight", help="Path to weight file.", required=False, type=str, default="synet.bin")
-	parser.add_argument("-i", "--image", help="Path to image file.", required=False, type=str, default="face_000.jpg")
+	parser.add_argument("-b", "--bin", help="Directory with binary files.", required=False, type=str, default="")
+	parser.add_argument("-m", "--model", help="Path to model file.", required=False, type=str, default="./data/precision/detection/test_003/sy_fp32_v0.xml")
+	parser.add_argument("-w", "--weight", help="Path to weight file.", required=False, type=str, default="./data/precision/detection/test_003/synet.bin")
+	parser.add_argument("-i", "--image", help="Path to image file.", required=False, type=str, default="./data/images/faces/faces_000.jpg")
 	args = parser.parse_args()
-	
+
 	Simd.Lib.Init(args.bin)
 	Synet.Lib.Init(args.bin)
 	
-	print("Synet version: {0}. \n".format(Synet.Lib.Version()))
+	print("Synet: {0}; {1} \n".format(Synet.Lib.Version(), Simd.Lib.SysInfo()))
 	
 	network = Synet.Network()
 	
+	print("Load network from {0} and {1}: ".format(args.model, args.weight), end="")
 	if not network.Load(args.model, args.weight) :
+		print("Error. Can't load!")
 		return 1
-	print("Load network from {0} and {1}.".format(args.model, args.weight))
+	print("OK.")
 
 	print("Set batch = 1.")	
 	network.SetBatch(1)
 	
-	print("Network inputs:")
+	print("Network has {0} inputs:".format(network.SrcSize()))
 	for src in range(network.SrcSize()) :
 		tensor = network.Src(src)
 		print(" {0}: {1} {2} {3} {4}".format(src, tensor.Name(), tensor.Shape(), tensor.Type().name, tensor.Format().name))
 		
-	print("Network outputs:")
+	print("Network has {0} outputs:".format(network.DstSize()))
 	for dst in range(network.DstSize()) :
 		tensor = network.Dst(dst)
 		print(" {0}: {1} {2} {3} {4}".format(dst, tensor.Name(), tensor.Shape(), tensor.Type().name, tensor.Format().name))
 
+	print("\nLoad test image {0} :".format(args.image), end="")	
 	image = Simd.Image()
 	if not image.Load(args.image) :
+		print("Error. Can't load!")
 		return 1
-	print("Load test image {0}.".format(args.image))	
+	print("OK.")
 	
+	print("Set network input: ", end = "")	
 	shape = network.NchwShape()
 	resized = Simd.Resized(image, shape[3], shape[2], Simd.ResizeMethod.Area)
 	lower = [0.0, 0.0, 0.0]
-	upper = [1.0, 1.0, 1.0]
+	upper = [255.0, 255.0, 255.0]
 	input = network.Src(0)
 	Simd.SynetSetInput(resized, lower, upper, input.Data(), shape[1], input.Format())
-	print("Set network input.")	
+	print("OK.")	
 	
 	network.Forward()
 	print("Network inference.")		
 	
-	print("Network output:")	
-	dst = network.DstByName("output").As32f()
-	for offs in range(5) :
-		print(" {0} ".format(dst[offs]))
-	print(" ... ")
+	dst = network.Dst(0)
+	shp = dst.Shape()
+	print("Network output {0} :".format(shp))	
+	ptr = dst.As32f()
+	for i in range(shp[2]) :
+		if ptr[i * 7 + 2] < 0.5 :
+			break
+		print(" {0} : {1} {2} {3} {4} {5} {6} {7} ".format(i, ptr[i*7 + 0], ptr[i*7 + 1], ptr[i*7 + 2], ptr[i*7 + 3], ptr[i*7 + 4], ptr[i*7 + 5], ptr[i*7 + 6]))
 	
-	print("\nSynet Python Wrapper ended successfully!")
+	print("\nSynet Python Wrapper ended successfully!\n")
 	
 	return 0
 	
