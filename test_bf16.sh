f022a35a42ae3456f185df00970a58ccd4d1d01c
Yermalayeu Ihar <ermig@tut.by>
2024-11-24 13:47:55 +0300

    +add legacy BF16 test inference_engine/test_003f.

--- test_bf16.sh
+++ test_bf16.sh
@@ -80,6 +80,7 @@ if [ $? -ne 0 ];then echo "Test $DIR is failed!"; exit; fi
 #TEST synet "" dn_test_000 human_vehicle 50 0 10 0 000 2
 #TEST synet "" ie_test_000 human_vehicle 500 0 1 1 000 2
 #TEST synet "" ie_test_001 face 500 0 10 1 000 2
-TEST synet "" ie_test_002 faces 20 1 1 1 000 2
+#TEST synet "" ie_test_002 faces 20 1 1 1 000 2
+TEST synet "" ie_test_003f faces 50 1 1 1 000 2
 
 exit
