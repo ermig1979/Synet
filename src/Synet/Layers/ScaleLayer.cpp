e91d0c59b047798f496ab8ff614f5ecf2a37c79f
Yermalayeu Ihar <ermig@tut.by>
2025-03-13 08:40:52 +0300

    +add optimizations of ScaleLayer (fix bugs).

--- src/Synet/Layers/ScaleLayer.cpp
+++ src/Synet/Layers/ScaleLayer.cpp
@@ -234,7 +234,6 @@ namespace Synet
         }
         if (_src16b || _dst16b)
         {
-            _scale16b.Init(_channels, _height * _width, src[0]->GetType(), dst[0]->GetType(), _format, true, _biasTerm);
             if (_biasTerm)
                 _shift.Share(this->Weight()[1]);
             else
@@ -289,6 +288,8 @@ namespace Synet
         }
         else if (src[0] != dst[0])
         {
+            if (_src16b || _dst16b)
+                _scale16b.Init(_channels, _height * _width, src[0]->GetType(), dst[0]->GetType(), _format, true, _biasTerm);
             if(_dst16b)
                 dst[0]->Reshape(TensorType16b, src[0]->Shape(), _format);
             else
